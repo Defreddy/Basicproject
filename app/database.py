d92d470bd7d9ba5b2fefa5f85f956856745d39a9
Frederik <r0802241@student.thomasmore.be>
2022-11-11 17:57:12 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,10 +1,8 @@
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-sql_database_url= os.getenv("DB_CONN")
+sql_database_url= "mysql+mysqlconnector://root:abc123@localhost:3306/cve"
 
 engine = create_engine(sql_database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
