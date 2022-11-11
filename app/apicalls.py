a78f8df8d63ab296c7e00063c71ff5b787e0d1a5
Frederik <r0802241@student.thomasmore.be>
2022-11-11 16:03:00 +0100

    Database connections

--- app/apicalls.py
+++ app/apicalls.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/product")
 async def root():
@@ -10,7 +24,12 @@ async def root():
 async def root():
     return {"message": "This is a cve"}
 
-#@app.post("/cve")
-#async def root():
-#    return {"message": "Hello World"}
+@app.get("/cve/{cveName}", response_model=schemas.Cve)
+def read_user(cveName: str, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, cveName=cveName)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
 
