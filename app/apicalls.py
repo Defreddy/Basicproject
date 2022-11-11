730454e5426ff7e3a5f63bab47bceab6a77bcbc3
Frederik <r0802241@student.thomasmore.be>
2022-11-11 19:06:32 +0100

    Update apicalls.py

--- app/apicalls.py
+++ app/apicalls.py
@@ -3,29 +3,25 @@ from sqlalchemy.orm import Session
 import mysql.connector
 from mysql.connector import Error
 
-connection=None
 
-try:
-    connection = mysql.connector.connect(host='localhost',
+
+connection = mysql.connector.connect(host='localhost',
                                          database='cve',
                                          user='root',
-                                         password='abc123')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
+                                         password='abc123')                                      
 
-except Error as e:
-    print("Error while connecting to MySQL", e)
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
-    
+db_Info = connection.get_server_info()
+
+print("Connected to MySQL Server version ", db_Info)
+
+cursor = connection.cursor()
+
+cursor.execute("select database();")
+
+record = cursor.fetchone()
+
+print("You're connected to database: ", record)
+
 #from . import crud, model, schema
 #from .database import SessionLocal, engine
 
