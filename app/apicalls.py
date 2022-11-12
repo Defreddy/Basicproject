10f5145907514a2169f5cb1b83cc43613d8d0862
Frederik <r0802241@student.thomasmore.be>
2022-11-13 00:56:32 +0100

    Update apicalls.py

--- app/apicalls.py
+++ app/apicalls.py
@@ -25,7 +25,7 @@ async def root():
 
 @app.get("/cve/{cveID}", response_model=schema.Cve)
 def read_user(cveID: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, cveName=cveID)
+    db_user = crud.get_user(db, cveID=cveID)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
