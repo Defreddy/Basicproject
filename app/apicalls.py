2da11b437859dd08b7d0cb4849d65d818ce16c6f
Frederik <r0802241@student.thomasmore.be>
2022-11-12 22:57:44 +0100

    changes

--- app/apicalls.py
+++ app/apicalls.py
@@ -2,20 +2,50 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 import mysql.connector
 from mysql.connector import Error
+from sqlalchemy import create_engine
+
+# DEFINE THE DATABASE CREDENTIALS
+user = 'root'
+password = 'password'
+host = '127.0.0.1'
+port = 3306
+database = 'cve'
+  
+# PYTHON FUNCTION TO CONNECT TO THE MYSQL DATABASE AND
+# RETURN THE SQLACHEMY ENGINE OBJECT
+def get_connection():
+    return create_engine(
+        url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(
+            user, password, host, port, database
+        )
+    )
+  
+  
+if __name__ == '__main__':
+  
+    try:
+        
+        # GET THE CONNECTION OBJECT (ENGINE) FOR THE DATABASE
+        engine = get_connection()
+        print(
+            f"Connection to the {host} for user {user} created successfully.")
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
+  
 
 #from . import crud, model, schema
-from .database import SessionLocal, engine
+#from .database import SessionLocal, engine
 
-model.Base.metadata.create_all(bind=engine)
+#model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+#def get_db():
+#    db = SessionLocal()
+#    try:
+#        yield db
+#    finally:
+#        db.close()
 
 @app.get("/product")
 async def root():
--- app/database.py
+++ app/database.py
@@ -1,9 +1,41 @@
+#from sqlalchemy import create_engine
+#from sqlalchemy.ext.declarative import declarative_base
+#from sqlalchemy.orm import sessionmaker
+#
+#sql_database_url= "mysql+mysqlconnector://root:abc123@db/cve"
+#
+#engine = create_engine(sql_database_url)
+#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#Base = declarative_base()
+
+
+# IMPORT THE SQALCHEMY LIBRARY's CREATE_ENGINE METHOD
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-sql_database_url= "mysql+mysqlconnector://root:abc123@db/cve"
-
-engine = create_engine(sql_database_url)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+  
+# DEFINE THE DATABASE CREDENTIALS
+user = 'root'
+password = 'password'
+host = '127.0.0.1'
+port = 3306
+database = 'cve'
+  
+# PYTHON FUNCTION TO CONNECT TO THE MYSQL DATABASE AND
+# RETURN THE SQLACHEMY ENGINE OBJECT
+def get_connection():
+    return create_engine(
+        url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(
+            user, password, host, port, database
+        )
+    )
+  
+  
+if __name__ == '__main__':
+  
+    try:
+        
+        # GET THE CONNECTION OBJECT (ENGINE) FOR THE DATABASE
+        engine = get_connection()
+        print(
+            f"Connection to the {host} for user {user} created successfully.")
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
