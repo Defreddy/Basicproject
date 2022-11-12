aa99205778440f3e9c3464966f34467aac4caeaa
Frederik <r0802241@student.thomasmore.be>
2022-11-13 00:51:44 +0100

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -2,8 +2,8 @@ from sqlalchemy.orm import Session
 from . import model, schema
 
 
-def get_user(db: Session, cveName: str):
-    return db.query(model.Cve).filter(model.Cve.cveName == cveName).first()
+def get_user(db: Session, cveID: int):
+    return db.query(model.Cve).filter(model.Cve.cveID == cveID).first()
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(model.Cve).offset(skip).limit(limit).all()
