5cf1d2ebf9f95f1343a7ba310a98f9656f5a2441
Frederik <r0802241@student.thomasmore.be>
2022-11-13 01:02:58 +0100

    ok

--- app/schema.py
+++ app/schema.py
@@ -1,8 +1,7 @@
 from datetime import date
 from pydantic import BaseModel
 
-class Cve(BaseModel):
-    cveID = int
+class CveBase(BaseModel):
     cveName = str
     vendorProject = str
     product = str
@@ -12,6 +11,13 @@ class Cve(BaseModel):
     requiredAction = str
     dueDate = date
     notes = str
+
+
+class CveCreate(CveBase):
+    pass
+
+class Cve(CveBase):
+    cveID = int
 
     class Config:
         orm_mode = True
