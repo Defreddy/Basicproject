35b931797adfffd99058135ad55a1d9e149e92bc
Frederik <r0802241@student.thomasmore.be>
2022-11-13 02:12:46 +0100

    Done

--- app/apicalls.py
+++ app/apicalls.py
@@ -15,14 +15,6 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/product")
-async def root():
-    return {"message": "This is a product"}
-
-@app.get("/cve")
-async def root():
-    return {"message": "This is a cve"}
-
 @app.get("/cve/{cveID}", response_model=schema.Cve)
 def read_user(cveID: int, db: Session = Depends(get_db)):
     db_user = crud.get_cveid(db, cveID=cveID)
@@ -37,7 +29,7 @@ def read_user(cveName: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
-@app.get("/users/", response_model=list[schema.Cve])
+@app.get("/allcve/", response_model=list[schema.Cve])
 def read_users(db: Session = Depends(get_db)):
     users = db.query(model.Cve).all()
     return users
