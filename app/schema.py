e574ceea6e696e1bc216bf4ed31c19a373eaf683
Frederik <r0802241@student.thomasmore.be>
2022-11-13 01:09:38 +0100

    Update schema.py

--- app/schema.py
+++ app/schema.py
@@ -2,22 +2,22 @@ from datetime import date
 from pydantic import BaseModel
 
 class CveBase(BaseModel):
-    cveName = str
-    vendorProject = str
-    product = str
-    vulnerabilityName = str
-    dateAdded = date
-    shortDescription = str
-    requiredAction = str
-    dueDate = date
-    notes = str
+    cveName: str
+    vendorProject: str
+    product: str
+    vulnerabilityName: str
+    dateAdded: date
+    shortDescription: str
+    requiredAction: str
+    dueDate: date
+    notes: str
 
 
 class CveCreate(CveBase):
     pass
 
 class Cve(CveBase):
-    cveID = int
+    cveID: int
 
     class Config:
         orm_mode = True
