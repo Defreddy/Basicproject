d705a064f1fb84678d0dc8b9d4497b3b787f984e
Frederik <r0802241@student.thomasmore.be>
2022-11-18 14:40:05 +0100

    Update apicalls.py

--- app/apicalls.py
+++ app/apicalls.py
@@ -54,7 +54,7 @@ def read_cveName(cveName: str, db: Session = Depends(get_db)):
 
 @app.get("/product/", response_model=schema.Cve)
 def read_users(request: Request, db: Session = Depends(get_db), query: Optional[str] = None):
-    products = get_cveProduct(query, db=db)
+    products = db.get_cveProduct(query, db=db)
     return {"request": request, "jobs": products}
 
 #def search(request: Request, db: Session = Depends(get_db), query: Optional[str] = None
