8c69ad955225c0f84a76a0388299d02af8b5acae
Frederik <r0802241@student.thomasmore.be>
2022-11-20 21:37:59 +0100

    Update apicalls.py

--- app/apicalls.py
+++ app/apicalls.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=['GET','POST'],
-    allow_headers=['Permissions-Policy: interest-cohort=()'],
+    allow_headers=['*'],
 )
 
 def get_db():
